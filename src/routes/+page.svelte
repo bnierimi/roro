d950fd0e8a08a64dc92aa0de39e47cafd86616a1
reb-el <shemiradiance@gmail.com>
2023-05-21 04:57:33 +0100

    (+) Fixed and Added some styles to the dashboard design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,15 +36,20 @@
 		<aside class="md:w-4/12 lg:w-3/12 h-full bg-gray-50/10 rounded-lg hidden space-y-2 md:flex flex-col focus:outline-violet-400 focus:ring-none">
 			<nav class="flex items-start justify-between p-3 pb-2 bg-violet-">
 				<!-- <button class="focus:outline-none focus:ring-transparent focus:outline-violet-400"> -->
-				<button class="focus:outline-none focus:ring-transparent focus:outline-violet-400">
+				<a href="http://twitter.com/bnierimi" class="focus:outline-none focus:ring-transparent focus:outline-violet-400">
 					<div class="flex space-x-2 bg-gray- p-1 rounded-md">
-						<div class="rounded-full w-12 h-12 bg-violet-600"></div>
+						<div class="rounded-full w-12 h-12 bg-violet- border-2 border-violet-600 p-1">
+							<img src="/imgs/avatars/001.jpg" alt="avatar" class="w-96 h-full object-cover transition-all duration-1000 rounded-full">
+						</div>
 						<div class="text-start">
 							<h1 class="font-medium text-lg">Yonko::Eri</h1>
-							<a href="http://twitter.com/bnierimi" class="focus:ring-transparent focus:outline-violet-400"><p class="text-violet-600 hover:text-violet-500">@bnierimi</p></a>
+							<!-- <a href="http://twitter.com/bnierimi" class="focus:ring-transparent focus:outline-violet-400"> -->
+								<!-- <p class="text-violet-600 hover:text-violet-500">@bnierimi</p> -->
+								<p class="text-violet-600">@bnierimi</p>
+							<!-- </a> -->
 						</div>
 					</div>
-				</button>
+				</a>
 				<!-- <h2 class="font-medium text-xl">n/Home</h2> -->
 			</nav>
 
@@ -85,25 +90,29 @@
 			<!-- Nav bar on small screen -->
 			<nav class="flex md:hidden items-start justify-between p-3 bg-violet-">
 				<!-- <button class="focus:outline-none focus:ring-transparent focus:outline-violet-400"> -->
-					<button class="focus:outline-violet-400 focus:ring-none">
-						<div class="flex space-x-2 bg-gray- p-1 rounded-md">
-							<div class="rounded-full w-12 h-12 bg-violet-600"></div>
+					<a href="http://twitter.com/bnierimi" class="focus:outline-violet-400 focus:ring-none">
+						<div class="flex items-center space-x-2 bg-gray- p-1 rounded-md">
+							<div class="rounded-full w-32 h-32 bg-violet- border-2 border-violet-600 p-1">
+								<img src="/imgs/avatars/001.jpg" alt="avatar" class="w-96 h-full object-cover transition-all duration-1000 rounded-full">
+							</div>
 							<div class="text-start">
 								<h1 class="font-medium text-lg">Yonko::Eri</h1>
-								<a href="https://twitter.com/bnierimi" class="focus:ring-transparent focus:outline-violet-400"><p class="text-violet-600 hover:text-violet-500">@bnierimi</p></a>
+								<!-- <a href="https://twitter.com/bnierimi" class="focus:ring-transparent focus:outline-violet-400"> -->
+									<p class="text-violet-600">@bnierimi</p>
+								<!-- </a> -->
 							</div>
 						</div>
-					</button>
+					</a>
 					
-					<button class="focus:outline-violet-400 focus:ring-none rounded-full p-1.5 hover:bg-base/50">
+					<!-- <button class="focus:outline-violet-400 focus:ring-none rounded-full p-1.5 hover:bg-base/50">
 						<i class="icon icon-ic_fluent_more_horizontal_20_regular flex text-lg"></i>
-					</button>
+					</button> -->
 					<!-- <h2 class="font-medium text-xl">n/Home</h2> -->
 			</nav>
 
 			<!-- Contact -->
 			<section id="contact" class="w-full p-4 space-y-2">
-				<h3 class="font-medium text-xl">/contact</h3>
+				<h3 class="font-medium text-xl opacity-70">$contact</h3>
 				<div class="flex items-center space-x-2">
 					<a href="https://twitter.com/bnierimi" class="rounded-full">
 						<div class="w-7 h-7 bg-violet-600 hover:bg-violet-700 flex items-center justify-center rounded-full transition-all duration-300">
@@ -120,11 +129,11 @@
 							<i class="mdi mdi-github text-xl rounded-full "></i>
 						</div>
 					</a>
-					<a href="https://bluesky.com/bnierimi" class="rounded-full">
+					<!-- <a href="https://bluesky.com/bnierimi" class="rounded-full">
 						<div class="w-7 h-7 bg-violet-600 hover:bg-violet-700 flex items-center justify-center rounded-full transition-all duration-300">
 							<i class="icon icon-ic_fluent_cloud_20_regular flex text-xl rounded-full "></i>
 						</div>
-					</a>
+					</a> -->
 					<!-- <a href="/" class="rounded-full">
 						<div class="w-7 h-7 bg-violet-600 hover:bg-violet-700 flex items-center justify-center rounded-full transition-all duration-300">
 							<i class="mdi mdi-discord text-xl rounded-full "></i>
@@ -135,11 +144,11 @@
 			
 			<!-- List of projects Section -->
 			<section id="probee" class="w-full h-full p-4 space-y-3">
-				<h3 class="font-medium text-xl">/projects</h3>
+				<h3 class="font-medium text-xl opacity-70">$projects</h3>
 				<div class="w-full h- grid grid-cols-2 md:grid-cols-3 gap-4">
 					{#each listOfProbees as eachProbee, index}
 						<a href={`probee/${eachProbee.id}`} class="rounded-lg focus:outline-violet-400 focus:ring-none">
-							<div class="relative rounded-lg ring-2 ring-transparent hover:ring-violet-500 mb-4 ">
+							<div class="relative rounded-lg ring-2 ring-transparent hover:ring-violet-500 ">
 								<img src={`/imgs/thumbnail-${eachProbee.img}`} alt={`thumbnail-${eachProbee.img}`} class="max-w-full h-auto rounded-lg">
 								<p class="p-2 rounded-lg font-medium">probee/{eachProbee.id}</p>
 							</div>
